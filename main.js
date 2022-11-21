947e6d8175cce58e6080cd7394b51911a1b693ae
Daniel <angelicodaniel93@gmail.com>
2022-11-21 16:50:22 +0100

    last commit

--- main.js
+++ main.js
@@ -13,14 +13,21 @@ var app = new Vue({
   
     },
     methods: {
-        randomMailGenerator (){
+        randomMail (){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
                 console.log(response.data.response)
                 this.newArr.push(response.data.response)
                 console.log(this.newArr)
             })
+        },
+        mailGenerator (){
+            for (let index = 0; index < 10; index++) {
+                this.randomMail()
+                
+            }
         }
+
     }
   })
   
